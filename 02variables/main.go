969e92e7ec0beb5a352becbcf50e7767536f5d18
REDDY <reddy@APPLEs-MacBook-Pro-2.local>
2024-10-13 22:16:06 +0530

    feat:new

---
+++ 02variables/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	var username string = ""
+	fmt.Println(username)
+	fmt.Printf("variable is of type: %T \n", username)
+
+	var isLoggedin bool
+	fmt.Println(isLoggedin)
+	fmt.Printf("variable is of type: %T \n", isLoggedin)
+}
